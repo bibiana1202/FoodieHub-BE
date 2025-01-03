49de1063506565507869babdeba18b962cc9114d
bibiana <wew1202@naver.com>
2025-01-03 16:18:02 +0900

    feat:식당정보수정 가게,메뉴

--- foodiehub/src/main/java/com/cherrymango/foodiehub/controller/api/MenuApiController.java
+++ foodiehub/src/main/java/com/cherrymango/foodiehub/controller/api/MenuApiController.java
@@ -7,12 +7,16 @@ import com.cherrymango.foodiehub.dto.MenuResponseDto;
 import com.cherrymango.foodiehub.dto.UpdateMenuRequestDto;
 import com.cherrymango.foodiehub.repository.StoreRepository;
 import com.cherrymango.foodiehub.service.MenuService;
+import com.cherrymango.foodiehub.service.StoreService;
+import com.cherrymango.foodiehub.util.TokenUtil;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.security.Principal;
 import java.util.List;
 
 @RestController
@@ -21,17 +25,50 @@ import java.util.List;
 public class MenuApiController {
     private final MenuService menuService;
     private final StoreRepository storeRepository;
+    final private StoreService storeService;
+    private final TokenUtil tokenUtil;
 
-    @PostMapping("/menu/{storeId}")
-    public ResponseEntity<MenuResponseDto> addMenu(@PathVariable("storeId") Long storeId, @RequestBody @Valid AddMenuRequestDto request) {
-        Long id = menuService.save(storeId, request);
-        Menu findMenu = menuService.findOne(id);
-        return ResponseEntity.status(HttpStatus.CREATED)
-                .body(new MenuResponseDto(findMenu.getId(), findMenu.getName(), findMenu.getPrice()));
+//    @PostMapping("/menu/{storeId}")
+//    public ResponseEntity<MenuResponseDto> addMenu(@PathVariable("storeId") Long storeId, @RequestBody @Valid AddMenuRequestDto request) {
+//        Long id = menuService.save(storeId, request);
+//        Menu findMenu = menuService.findOne(id);
+//        return ResponseEntity.status(HttpStatus.CREATED)
+//                .body(new MenuResponseDto(findMenu.getId(), findMenu.getName(), findMenu.getPrice()));
+//    }
+
+//    @GetMapping("/menu/{storeId}")
+//    public ResponseEntity<List<MenuResponseDto>> findAllMenus(@PathVariable("storeId") Long storeId) {
+//        Store store = storeRepository.findById(storeId).get();
+//        List<MenuResponseDto> menus = menuService.findMenus(store).stream()
+//                .map(menu -> new MenuResponseDto(menu.getId(), menu.getName(), menu.getPrice()))
+//                .toList();
+//        return ResponseEntity.ok()
+//                .body(menus);
+//    }
+
+//    @PutMapping("/menu")
+//    public ResponseEntity<MenuResponseDto> updateMenu(@RequestBody @Valid UpdateMenuRequestDto request) {
+//        Long id = menuService.update(request);
+//        Menu findMenu = menuService.findOne(id);
+//        return ResponseEntity.ok()
+//                .body(new MenuResponseDto(findMenu.getId(), findMenu.getName(), findMenu.getPrice()));
+//    }
+
+    @DeleteMapping("/menu/{id}")
+    public ResponseEntity<Void> deleteMenu(@PathVariable("id") Long id) {
+        menuService.delete(id);
+        return ResponseEntity.ok().build();
     }
 
-    @GetMapping("/menu/{storeId}")
-    public ResponseEntity<List<MenuResponseDto>> findAllMenus(@PathVariable("storeId") Long storeId) {
+
+    // 식당 수정 (메뉴 로드) GET (박혜정 2025-01-03)
+    @GetMapping("/menu")
+    public ResponseEntity<List<MenuResponseDto>> findAllMenus(Principal principal, HttpServletRequest request) {
+        Long userId = tokenUtil.getSiteUserIdOrThrow(principal, request);
+        // userId로 storeId 찾기
+        Long storeId = storeService.getStoreIdByUserId(userId);
+
+
         Store store = storeRepository.findById(storeId).get();
         List<MenuResponseDto> menus = menuService.findMenus(store).stream()
                 .map(menu -> new MenuResponseDto(menu.getId(), menu.getName(), menu.getPrice()))
@@ -40,18 +77,26 @@ public class MenuApiController {
                 .body(menus);
     }
 
+    // 식당 수정 (메뉴 등록) POST (박혜정 2025-01-03)
+    @PostMapping("/menu")
+    public ResponseEntity<MenuResponseDto> addMenu(@RequestBody @Valid AddMenuRequestDto addMenuRequestDto,Principal principal, HttpServletRequest request) {
+        Long userId = tokenUtil.getSiteUserIdOrThrow(principal, request);
+        // userId로 storeId 찾기
+        Long storeId = storeService.getStoreIdByUserId(userId);
+
+        Long id = menuService.save(storeId, addMenuRequestDto);
+        Menu findMenu = menuService.findOne(id);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(new MenuResponseDto(findMenu.getId(), findMenu.getName(), findMenu.getPrice()));
+    }
+
+    // 식당 수정 (메뉴 수정) PUT (박혜정 2025-01-03)
     @PutMapping("/menu")
     public ResponseEntity<MenuResponseDto> updateMenu(@RequestBody @Valid UpdateMenuRequestDto request) {
         Long id = menuService.update(request);
         Menu findMenu = menuService.findOne(id);
         return ResponseEntity.ok()
                 .body(new MenuResponseDto(findMenu.getId(), findMenu.getName(), findMenu.getPrice()));
-    }
-
-    @DeleteMapping("/menu/{id}")
-    public ResponseEntity<Void> deleteMenu(@PathVariable("id") Long id) {
-        menuService.delete(id);
-        return ResponseEntity.ok().build();
     }
 
 }
--- foodiehub/src/main/java/com/cherrymango/foodiehub/controller/api/StoreApiController.java
+++ foodiehub/src/main/java/com/cherrymango/foodiehub/controller/api/StoreApiController.java
@@ -12,11 +12,14 @@ import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.MalformedURLException;
 import java.security.Principal;
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -44,17 +47,6 @@ public class StoreApiController {
         return ResponseEntity.ok(storeFavoriteList);
     }
 
-    @GetMapping("/mystore/details/{userId}")
-    public ResponseEntity<MyStoreResponseDto> getMyStoreDetails(@PathVariable("userId") Long userId) {
-        MyStoreResponseDto myStoreDetails = storeService.getMyStoreDetails(userId);
-
-        if (myStoreDetails == null) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
-        }
-
-        return ResponseEntity.ok(myStoreDetails);
-    }
-
     @GetMapping("/store/category/{category}")
     public ResponseEntity<List<StoreListItemResponseDto>> getStoreByCategory(@PathVariable("category") Category category,
                                                                              @RequestParam(value = "limit", required = false, defaultValue = "0") int limit) {
@@ -69,22 +61,67 @@ public class StoreApiController {
         return ResponseEntity.ok(stores);
     }
 
-    // 가게 정보 저장
-    /*
-    *     @PostMapping("/save/{userId}")
-    public String postSaveStore(@ModelAttribute @Valid AddStoreRequestDto addStoreRequestDto, @PathVariable("userId") Long userId) {
-        storeService.register(userId, addStoreRequestDto);
-        return "redirect:/";
-    }
-    * */
+    // 가게 정보 저장 (박혜정 2025-01-02)
     @PostMapping("/store/save")
     public ResponseEntity<Long> storeSave(@ModelAttribute @Valid AddStoreRequestDto addStoreRequestDto, Principal principal, HttpServletRequest request){
-        System.out.println("storeSave!!!!!!!!");
         try {
             Long userId = tokenUtil.getSiteUserIdOrThrow(principal, request);
-            System.out.println("userId = " + userId);
             Long storeId = storeService.register(userId, addStoreRequestDto);
-            System.out.println("storeId = " + storeId);
+            return ResponseEntity.ok(storeId);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null);
+        }
+    }
+
+    // 내 식당 확인 (박혜정 2025-01-03)
+    @GetMapping("/mystore/details")
+    public ResponseEntity<?> getMyStoreDetails(Principal principal, HttpServletRequest request) {
+        try {
+            Long userId = tokenUtil.getSiteUserIdOrThrow(principal, request);
+            MyStoreResponseDto myStoreDetails = storeService.getMyStoreDetails(userId);
+            if (myStoreDetails == null) {
+                return ResponseEntity.ok(Collections.singletonMap("isStore", false));
+            }
+//            return ResponseEntity.ok(myStoreDetails);
+            return ResponseEntity.ok(Map.of(
+                    "isStore", true,
+                    "details", myStoreDetails
+            ));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                    .body(Collections.singletonMap("message", "권한이 없습니다."));
+        }
+    }
+
+    // 식당 정보 수정 GET (박혜정 2025-01-03)
+    @GetMapping("/store/update")
+    public ResponseEntity<?> getUpdateStore(Principal principal, HttpServletRequest request) {
+        try {
+            Long userId = tokenUtil.getSiteUserIdOrThrow(principal, request);
+            // userId로 storeId 찾기
+            Long storeId = storeService.getStoreIdByUserId(userId);
+            UpdateStoreDetailDto store = storeService.getUpdateDetails(storeId);
+            if (store == null) {
+                return ResponseEntity.ok(Collections.singletonMap("isStore", false));
+            }
+            return ResponseEntity.ok(Map.of(
+                    "isStore", true,
+                    "store", store
+            ));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                    .body(Collections.singletonMap("message", "권한이 없습니다."));
+        }
+    }
+
+    // 식당 정보 수정 POST (박혜정 2025-01-03)
+    @PutMapping("/store/update")
+    public ResponseEntity<Long>  postUpdateStore(@RequestBody @Valid UpdateStoreRequestDto updateStoreRequestDto, Principal principal, HttpServletRequest request) {
+        try {
+            Long userId = tokenUtil.getSiteUserIdOrThrow(principal, request);
+            // userId로 storeId 찾기
+            Long storeId = storeService.getStoreIdByUserId(userId);
+            storeService.update(storeId, updateStoreRequestDto);
             return ResponseEntity.ok(storeId);
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null);
